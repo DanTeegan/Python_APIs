0433a3e5ca7193a8075f05ab88a9de0690b4e1e9
dan. teegan <dan.teegan@hotmail.com>
2020-07-07 12:34:58 +0100

    Python API

--- python_requests_module.py
+++ python_requests_module.py
@@ -10,17 +10,24 @@
 import requests
 
 # Check HTTP/HTTPS 200 = Success - 400 - 404 page not found.
-response_bbc = requests.get("https://www.bbc.co.uk/") # Here we have created a variable.
+# response_bbc = requests.get("https://www.bbc.co.uk/") # Here we have created a variable.
+post_codes_req = requests.get ("https://api.postcodes.io/postcodes/N135PG")
+print(post_codes_req.status_code)
 
-print(response_bbc.status_code) # status.code presents the data in more of a readable format
+if post_codes_req.status_code == 200:
+    print("Sucess")
+elif post_codes_req.status_code == 400:
+    print("sorry not available")
 
-print(response_bbc.headers) # headers displays information that is in the header
-
-print(type(response_bbc.headers)) # Using the type method
-
-print(response_bbc.keys) # To find the keys within the dictionary
-
-print(response_bbc.content) # .content will display the content
+# print(response_bbc.status_code) # status.code presents the data in more of a readable format
+#
+# print(response_bbc.headers) # headers displays information that is in the header
+#
+# print(type(response_bbc.headers)) # Using the type method
+#
+# print(response_bbc.keys) # To find the keys within the dictionary
+#
+# print(response_bbc.content) # .content will display the content
 
 # Iteration 1
 # Receive a response code and check if 200 - print success
@@ -36,27 +43,27 @@ print(response_bbc.content) # .content will display the content
 
 # Control flow of checking the status_code of a website (www.bbc.co.uk)
 # Iteration 1
-if response_bbc.status_code == 200:
-    print("Success your in!")
-elif response_bbc.status_code == 400:
-    print("Page not found")
-elif response_bbc.status_code == 404:
-    print("oops sorry something went wrong")
-else:
-    print("Could not identify the status code, Please try another website")
-
-# Creating a function called check_response_code to check the status_code
-# Iteration 2
-def check_response_code():
-    if response_bbc.status_code == 200:
-        print("Success your in!")
-    elif response_bbc.status_code == 400:
-        print("Page not found")
-    elif response_bbc.status_code == 404:
-        print("oops sorry something went wrong")
-    else:
-        print("Could not identify the status code, Please try another website")
-
-# Iteration 3
-from python_request_module import check_response_code
-print(check_response_code())
+# if response_bbc.status_code == 200:
+#     print("Success your in!")
+# elif response_bbc.status_code == 400:
+#     print("Page not found")
+# elif response_bbc.status_code == 404:
+#     print("oops sorry something went wrong")
+# else:
+#     print("Could not identify the status code, Please try another website")
+#
+# # Creating a function called check_response_code to check the status_code
+# # Iteration 2
+# def check_response_code():
+#     if response_bbc.status_code == 200:
+#         print("Success your in!")
+#     elif response_bbc.status_code == 400:
+#         print("Page not found")
+#     elif response_bbc.status_code == 404:
+#         print("oops sorry something went wrong")
+#     else:
+#         print("Could not identify the status code, Please try another website")
+#
+# # Iteration 3
+# from python_request_module import check_response_code
+# print(check_response_code())
